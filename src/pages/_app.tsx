977d758ab9b0f132d1ca3c160e8e23bf28511138
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-01-07 19:36:59 +0800

    Add initial components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import { Reset } from 'styled-reset';
+import theme from '../theme';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <Reset />
+    <Head>
+      <title>FSAYCON.DEV - Personal Website of Franrey Saycon</title>
+      <meta
+        name="description"
+        content="Hi there, I'm Franrey Saycon. I'm a software engineer specializing in architecting solutions
+      and building website/mobile apps."
+      />
+      <link rel="canonical" href="https://fsaycon.dev/" />
+      <meta name="robots" content="index, follow" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="FSAYCON.DEV - Personal Website of Franrey Saycon" />
+      <meta property="twitter:card" content="summary" />
+      <meta
+        property="og:description"
+        content="Hi there, I'm Franrey Saycon. I'm a software engineer specializing in architecting solutions
+      and building website/mobile apps."
+      />
+      <meta property="og:image" content="https://i.ibb.co/Y7bYd27/preview-image.png" />
+      <meta property="og:image:alt" content="Fantasy drawing of Franrey Saycon in a sci-fi mechanic suit solving a cosmic rubix cube." />
+      <meta property="og:url" content="https://fsaycon.dev/" />
+      <meta property="og:site_name" content="FSAYCON.DEV - Personal Website of Franrey Saycon" />
+      {/* <link rel="shortcut icon" href={Favicon} type="image/x-icon" />
+      <link rel="icon" href={Favicon} type="image/x-icon" /> */}
+    </Head>
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
+
+export default MyApp; 
---
+++ src/theme.ts
@@ -0,0 +1,37 @@
+const theme = {
+    fonts: {
+      blinker: 'Blinker, Arial, Helvetica, sans-serif',
+    },
+    fontSizes: [
+      '0.563rem',
+      '0.75rem',
+      '1rem',
+      '1.333rem',
+      '1.777rem',
+      '2.369rem',
+    ],
+    colors: {
+      blue: '#336bb8',
+      violet: '#41328f',
+      green: '#33888f',
+      purple: '#814d97',
+      white: '#eeeeee',
+      orange: '#faa945',
+    },
+    spaces: [
+      '0.563rem',
+      '0.75rem',
+      '1rem',
+      '1.333rem',
+      '1.777rem',
+      '2.369rem',
+    ],
+    breakpoints: [
+      '576px',
+      '768px',
+      '992px',
+      '1200px',
+    ],
+  };
+  
+  export default theme;
