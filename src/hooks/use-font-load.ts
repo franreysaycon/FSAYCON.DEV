705de36ec576fe18c6fe868951bfd48a0977c289
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-02-12 11:28:57 +0800

    Add font observer

---
+++ src/hooks/use-font-load.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import FontFaceObserver from "fontfaceobserver"
+
+interface FontStatus {
+  fontLoaded: boolean;
+}
+
+function useFontLoad(fonts: string[]): FontStatus  {
+
+    const [fontLoaded, setFontLoaded] = useState(false)
+
+    useEffect(() => {
+        const loadFonts = async (): Promise<void> => {
+            const observers = fonts.map(font => new FontFaceObserver(font))
+            const fontPromises = observers.map(obs => obs.load())
+            await Promise.all(fontPromises)
+            setFontLoaded(true)
+        }
+        loadFonts()
+    }, [fonts, fontLoaded]) 
+
+    return { fontLoaded }
+}
+
+export default useFontLoad
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,44 +4,54 @@ import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import { Reset } from 'styled-reset';
 import GlobalStyle from 'global-styles';
+import useFontLoad from 'hooks/use-font-load';
 import theme from '../theme';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <ThemeProvider theme={theme}>
-    <Reset />
-    <Head>
-      <title>FSAYCON.DEV - Personal Website of Franrey Saycon</title>
-      <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-regular.woff2" as="font" type="font/woff2" />
-      <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-regular.woff" as="font" type="font/woff" />
-      <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-bold.woff2" as="font" type="font/woff2" />
-      <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-bold.woff" as="font" type="font/woff" />
-      <link rel="preload" href="/fonts/staatliches/staatliches-regular.woff2" as="font" type="font/woff2" />
-      <link rel="preload" href="/fonts/staatliches/staatliches-regular.woff" as="font" type="font/woff" />
-      <meta
-        name="description"
-        content="Hi there, I'm Franrey Saycon. I'm a software engineer specializing in architecting solutions
-      and building website/mobile apps."
-      />
-      <link rel="canonical" href="https://fsaycon.dev/" />
-      <meta name="robots" content="index, follow" />
-      <meta property="og:type" content="article" />
-      <meta property="og:title" content="FSAYCON.DEV - Personal Website of Franrey Saycon" />
-      <meta property="twitter:card" content="summary" />
-      <meta
-        property="og:description"
-        content="Hi there, I'm Franrey Saycon. I'm a software engineer specializing in architecting solutions
-      and building website/mobile apps."
-      />
-      <meta property="og:image" content="/preview-image.png" />
-      <meta property="og:image:alt" content="Fantasy drawing of Franrey Saycon in a sci-fi mechanic suit solving a cosmic rubix cube." />
-      <meta property="og:url" content="https://fsaycon.dev/" />
-      <meta property="og:site_name" content="FSAYCON.DEV - Personal Website of Franrey Saycon" />
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-    </Head>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+
+  const { fontLoaded } = useFontLoad(['Barlow Condensed', 'Staatliches'])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <Head>
+        <title>FSAYCON.DEV - Personal Website of Franrey Saycon</title>
+        <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-regular.woff2" as="font" type="font/woff2" crossOrigin="*" />
+        <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-regular.woff" as="font" type="font/woff" crossOrigin="*" />
+        <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-bold.woff2" as="font" type="font/woff2" crossOrigin="*" />
+        <link rel="preload" href="/fonts/barlow-condensed/barlow-condensed-bold.woff" as="font" type="font/woff" crossOrigin="*" />
+        <link rel="preload" href="/fonts/staatliches/staatliches-regular.woff2" as="font" type="font/woff2" crossOrigin="*" />
+        <link rel="preload" href="/fonts/staatliches/staatliches-regular.woff" as="font" type="font/woff" crossOrigin="*" />
+        <meta
+          name="description"
+          content="Hi there, I'm Franrey Saycon. I'm a software engineer specializing in architecting solutions
+        and building website/mobile apps."
+        />
+        <link rel="canonical" href="https://fsaycon.dev/" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="FSAYCON.DEV - Personal Website of Franrey Saycon" />
+        <meta property="twitter:card" content="summary" />
+        <meta
+          property="og:description"
+          content="Hi there, I'm Franrey Saycon. I'm a software engineer specializing in architecting solutions
+        and building website/mobile apps."
+        />
+        <meta property="og:image" content="/preview-image.png" />
+        <meta property="og:image:alt" content="Fantasy drawing of Franrey Saycon in a sci-fi mechanic suit solving a cosmic rubix cube." />
+        <meta property="og:url" content="https://fsaycon.dev/" />
+        <meta property="og:site_name" content="FSAYCON.DEV - Personal Website of Franrey Saycon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <GlobalStyle />
+      {
+        fontLoaded && <Component {...pageProps} />
+      }
+    </ThemeProvider>
+  );
+}
+
+
 
 export default MyApp;
