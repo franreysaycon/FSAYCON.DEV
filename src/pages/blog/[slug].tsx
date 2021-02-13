39d91ea368ff986656282f9a3ed74452c1a0207e
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-02-13 18:47:22 +0800

    Move metahead

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,26 +1,18 @@
 import BlogApp from "components/blog"
 import { BlogPageProps } from "components/blog/types"
+import MetaHead from "components/common/MetaHead"
 import getAllPosts from "lib/get-all-posts"
 import getPostBySlug from "lib/get-post-by-slug"
 import { GetStaticPaths, GetStaticProps, NextPage } from "next"
-import Head from "next/head"
 import React from "react"
 
 const BlogPage: NextPage<BlogPageProps> = ({ content, data }) => (
   <>
-    <Head>
-      <title key="title">
-        FSAYCON.DEV -&nbsp;
-        {data.title}
-      </title>
-      <link rel="canonical" href={`https://fsaycon.dev/blog/${data.slug}`} />
-      <meta name="description" content={data.description} key="description" />
-      <meta property="og:title" content={data.title} key="og:title" />
-      <meta property="og:description" content={data.description} key="og:description" />    
-      <meta property="og:url" content={`https://fsaycon.dev/blog/${data.slug}`} key="og:url" />
-      <meta name="twitter:title" content={data.title} key="twitter:title" />
-      <meta name="twitter:description" content={data.description} key="twitter:description" />
-    </Head>
+    <MetaHead
+      title={`FSAYCON.DEV - ${data.title}`}
+      description={data.description}
+      link={`https://fsaycon.dev/blog/${data.slug}`} 
+    />
     <BlogApp
       content={content}
       title={data.title}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
-import Homepage from "components/homepage"
+import MetaHead from "components/common/MetaHead"
+import HomeApp from "components/homepage"
+import { BlogListProps } from "components/homepage/BlogList/types"
 import getAllPosts from "lib/get-all-posts"
 import { GetStaticProps } from "next"
+import React from "react"
+
+const Homepage: React.FC<BlogListProps> = ({ items }) => (
+  <>
+    <MetaHead
+      title="FSAYCON.DEV - Personal Website of Franrey Saycon"
+      description="Hi there, I'm Franrey Saycon. I'm a software engineer specializing in architecting solutions
+      and building website/mobile apps."
+      link="https://fsaycon.dev/" 
+    />
+    <HomeApp items={items} />
+  </>
+)  
 
 export const getStaticProps: GetStaticProps = async () => {
   const { items } = getAllPosts()
