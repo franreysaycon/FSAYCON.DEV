e2a91bb3d96ae65c72207d004f785a166ea2f8e0
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-03-03 15:30:28 +0800

    Add google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
@@ -7,6 +8,7 @@ import useFontLoad from 'hooks/use-font-load';
 import NavBar from 'components/common/NavBar';
 import Footer from 'components/common/Footer';
 import MetaHead from 'components/common/MetaHead';
+import Head from 'next/head';
 import theme from '../theme';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
@@ -16,6 +18,19 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ThemeProvider theme={theme}>
       <Reset />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PSKJ3PD');
+          `
+        }}
+        />
+      </Head>
       <MetaHead
         title={pageProps.meta?.title}
         description={pageProps.meta?.description}
@@ -26,6 +41,18 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
       {
         fontLoaded && (
           <>
+            <noscript>
+              <iframe
+                title="Google Tag Manager Body"
+                src="https://www.googletagmanager.com/ns.html?id=GTM-PSKJ3PD"
+                height="0"
+                width="0"
+                style={{
+                  display: "none",
+                  visibility:"hidden"
+                }}
+              />
+            </noscript>
             <NavBar />
             <Component {...pageProps} />
             <Footer />
