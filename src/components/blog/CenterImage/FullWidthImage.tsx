c88aaf28588e1ae380073a19df2a7752f8366f09
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-03-20 20:51:15 +0800

    Improve width on image

--- src/components/blog/CenterImage/FullWidthImage.tsx
+++ src/components/blog/CenterImage/FullWidthImage.tsx
@@ -2,7 +2,11 @@ import styled from "styled-components"
 
 const FullWidthImage = styled.img`
   margin-bottom: ${({ theme }) => theme.spaces[0]};
-  width: 70%;
+  width: 100%;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints[1]}) {
+    width: 70%;
+  }
 `
 
 export default FullWidthImage
